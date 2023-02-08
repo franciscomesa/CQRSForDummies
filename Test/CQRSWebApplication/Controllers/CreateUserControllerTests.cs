8da409e5ec397c3e66966b1ca192a8c596363e0b
Francisco Mesa <franciscomesa@gmail.com>
2023-02-08 14:02:01 +0000

    🚧 Add unit test

---
+++ Test/CQRSWebApplication/Controllers/CreateUserControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using CQRSWebApplication.Controllers;
+
+namespace Test.CQRSWebApplication.Controllers;
+
+public class CreateUserControllerTests
+{
+    [Fact]
+    public void CreateUserIsNotImplemented()
+    {
+        var createUSerController = new CreateUserController();
+        Action executeMethod = () => createUSerController.Execute();
+
+        var exception = Assert.Throws<NotImplementedException>(executeMethod);
+
+        Assert.Equal("The method or operation is not implemented.", exception.Message);
+        Assert.False(false);
+        
+        
+    }
+}
