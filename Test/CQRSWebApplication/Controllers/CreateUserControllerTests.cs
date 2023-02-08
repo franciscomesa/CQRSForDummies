841dcbc6183c2aebe2b12c2948eb8890e814d57c
Francisco Mesa <franciscomesa@gmail.com>
2023-02-08 14:58:58 +0000

    ✅ Check if Send is called

--- Test/CQRSWebApplication/Controllers/CreateUserControllerTests.cs
+++ Test/CQRSWebApplication/Controllers/CreateUserControllerTests.cs
@@ -1,6 +1,8 @@
-﻿using CQRSWebApplication.Common.CommandBus;
+﻿using CQRSWebApplication.Commands;
+using CQRSWebApplication.Common.CommandBus;
 using CQRSWebApplication.Controllers;
 using CQRSWebApplication.DTOs;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NSubstitute;
 
 namespace Test.CQRSWebApplication.Controllers;
@@ -8,18 +10,37 @@ namespace Test.CQRSWebApplication.Controllers;
 public class CreateUserControllerTests
 {
     [Fact]
+    public void FalseIsFalse()
+    {
+        Assert.False(false);        
+    }
+    
+    [Fact]
     public void CreateUserIsNotImplemented()
     {
-        var mockCommandBus = Substitute.For<CommandBus>();
-        
-        var createUSerController = new CreateUserController(mockCommandBus);
         var fooUSer = new User(1);
+        var mockCommandBus = Substitute.For<CommandBus>();
+        var createUSerController = new CreateUserController(mockCommandBus);
         Action executeMethod = () => createUSerController.Execute(fooUSer);
 
         var exception = Assert.Throws<NotImplementedException>(executeMethod);
 
         Assert.Equal("The method or operation is not implemented.", exception.Message);
-        Assert.False(false);
+    }
+
+    [Fact]
+    public void CreateUserSendCommandBus()
+    {
+        var fooUSer = new User(1);
+        var mockCommandBus = Substitute.For<CommandBus>();
+        var createUSerController = new CreateUserController(mockCommandBus);
+        Action executeMethod = () => createUSerController.Execute(fooUSer);
+
+        var exception = Assert.Throws<NotImplementedException>(executeMethod);
+        
+        mockCommandBus.Received().Send(Arg.Any<CreateUserCommand>()); // << This will fail if 2 or 4 calls were received
+
+        Assert.Equal("The method or operation is not implemented.", exception.Message);
     }
 }
 
